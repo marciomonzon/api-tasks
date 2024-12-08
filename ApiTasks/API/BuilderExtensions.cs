685409cf4354b9865022d5847c76278fc0dd0a27
marciomonzon <marcio.pcmonzon@gmail.com>
2024-12-08 13:38:04 -0300

    add extensions to organize program.cs class

---
+++ ApiTasks/API/BuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Application.UsersUseCase.Commands;
+using Application.UsersUseCase.Validators;
+using FluentValidation.AspNetCore;
+using FluentValidation;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public static class BuilderExtensions
+    {
+        public static void AddServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+        }
+
+        public static void AddDatabase(this WebApplicationBuilder builder)
+        {
+            var configuration = builder.Configuration;
+            builder.Services.AddDbContext<TasksDbContext>(options => options
+                .UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        public static void AddMediatrCommands(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddMediatR(config => 
+            config.RegisterServicesFromAssemblies(typeof(CreateUserCommand).Assembly));
+        }
+
+        public static void AddFluentValidationValidators(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddValidatorsFromAssemblyContaining<CreateUserCommandValidator>();
+            builder.Services.AddFluentValidationAutoValidation();
+        }
+    }
+}
--- ApiTasks/API/Program.cs
+++ ApiTasks/API/Program.cs
@@ -1,23 +1,14 @@
-using Application.UsersUseCase.Commands;
-using Application.UsersUseCase.Validators;
-using FluentValidation;
-using FluentValidation.AspNetCore;
-using Infrastructure.Persistence;
-using Microsoft.EntityFrameworkCore;
+using API;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<TasksDbContext>(options => options
-                .UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+builder.AddServices();
+builder.AddDatabase();
 
-builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(typeof(CreateUserCommand).Assembly));
+builder.AddMediatrCommands();
 
-builder.Services.AddValidatorsFromAssemblyContaining<CreateUserCommandValidator>();
-builder.Services.AddFluentValidationAutoValidation();
+builder.AddFluentValidationValidators();
 
 var app = builder.Build();
 
